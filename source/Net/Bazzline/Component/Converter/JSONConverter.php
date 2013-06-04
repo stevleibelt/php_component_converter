ffb0b653f0eebfc3e8a7ce23ab4ac2cb48c91b81
stevleibelt <stev@leibelt.de>
2013-06-04 23:39:42 +0200

    Added JSONConverter with tests

--- source/Net/Bazzline/Component/Converter/JSONConverter.php
+++ source/Net/Bazzline/Component/Converter/JSONConverter.php
@@ -1,12 +1,44 @@
 <?php
 /**
  * @author stev leibelt <artodeto@arcor.de>
- * @since 2013-06-02 
+ * @since 2013-06-04
  */
 
 namespace Net\Bazzline\Component\Converter;
 
+/**
+ * Class JSONConverter
+ *
+ * @package Net\Bazzline\Component\Converter
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-04
+ */
+class JSONConverter extends ConverterAbstract
+{
+    /**
+     * @{inheritDoc}
+     */
+    protected function convertFromSourceToArray($source)
+    {
+        return json_decode($source, true);
+    }
 
-class JSONConverter {
+    /**
+     * @{inheritDoc}
+     */
+    protected function convertFromArrayToSource(array $array)
+    {
+        return json_encode($array);
+    }
 
+    /**
+     * @{inheritDoc}
+     */
+    public function fromSource($source)
+    {
+        $this->source = $source;
+        $this->array = $this->convertFromSourceToArray($source);
+
+        return $this;
+    }
 }
--- test/Net/Bazzline/Component/Converter/JSONConverterTest.php
+++ test/Net/Bazzline/Component/Converter/JSONConverterTest.php
@@ -6,7 +6,95 @@
 
 namespace Net\Bazzline\Component\Converter;
 
+use PHPUnit_Framework_TestCase;
 
-class JSONConverterTest {
+/**
+ * Class JSONConverterTest
+ *
+ * @package Net\Bazzline\Component\Converter
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since
+ */
+class JSONConverterTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-04
+     */
+    private $array;
 
+    /**
+     * @var XmlConverter
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-04
+     */
+    private $converter;
+
+    /**
+     * @var string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-04
+     */
+    private $source;
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-04
+     */
+    protected function setUp()
+    {
+        $this->array = array(
+            'test' => array(
+                'first' => array(
+                    'firstChild' => 'firstChildValue'
+                ),
+                'second' => 'secondValue'
+            )
+        );
+
+        $this->source = '{"test":' .
+            '{"first":' .
+                '{"firstChild":"firstChildValue"},' .
+            '"second":"secondValue"' .
+            '}}';
+
+        $this->converter = new JSONConverter();
+    }
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-04
+     */
+    public function testFromSource()
+    {
+        $this->converter->fromSource($this->source);
+
+        $this->assertEquals(
+            $this->source,
+            $this->converter->toSource()
+        );
+        $this->assertEquals(
+            $this->array,
+            $this->converter->toPhpArray()
+        );
+    }
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-04
+     */
+    public function testFromPhpArray()
+    {
+        $this->converter->fromPhpArray($this->array);
+
+        $this->assertEquals(
+            $this->source,
+            $this->converter->toSource()
+        );
+        $this->assertEquals(
+            $this->array,
+            $this->converter->toPhpArray()
+        );
+    }
 }
